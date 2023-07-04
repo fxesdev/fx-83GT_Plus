63742bde64dc37822cdb1dd49832d8bf3ada8234
Fraser Price <fraserbcprice@gmail.com>
2023-07-04 14:48:20 +0100

    Timing recovery finished!

--- firmware/video_dump/timing_recovery.py
+++ firmware/video_dump/timing_recovery.py
@@ -8,9 +8,9 @@ cap = cv2.VideoCapture('video.mov')
 if cap.isOpened()== False:
 	print("Error opening video file")
 
-#num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-num_frames = 5000
+#num_frames = 2000
 
 #num_frames = 300
 #cap.set(cv2.CAP_PROP_POS_FRAMES, 1500)
@@ -18,12 +18,16 @@ num_frames = 5000
 # Get frame deltas
 deltas = []
 _, lframe = cap.read()  
-for x in range(num_frames):
+for x in range(num_frames - 1):
 	if x % 250 == 0:
 		print(f"[*] Calculating delta {x:05d}/{num_frames:05d}")
 
 	# Get the next frame
 	ret, frame = cap.read()
+
+	if ret == False:
+		print("[-] Error reading frame!")
+		quit()
 
 	# Calculate the average difference
 	diff = cv2.absdiff(lframe, frame)
@@ -44,9 +48,6 @@ print(f"[*] Applying filter kernel")
 filtered = np.concatenate(([0] * int(size/2), np.convolve(kernel, deltas, mode='valid'), [0] * int(size/2)))
 print(f"[*] Filter kernel applied")
 
-#for x in range(num_frames):
-#	print(f"{x} {start_frame+x:03d}->{start_frame+x+1:03d} {deltas[x]:10.7f} {filtered[x]:10.7f} {deltas[x] >= 5}")
-
 print(f"[*] Applying peak detection")
 # Peak detection
 # Get max value in window around sample
@@ -65,27 +66,24 @@ for x in range(1, len(filtered) - 1):
 peaks.append(0)	# Account for missing point at the end
 
 # Apply a threshold
-pthresh = 3
+pthresh = 2.7
 peaks = [x for x in peaks if filtered[x] >= pthresh]
-
-# Generate signal for graphing
-peaks_signal = np.zeros(len(filtered))
-
-for p in peaks:
-	peaks_signal[p] = 5
 
 # Merge close peaks
 dist = 3
 peaks_merged = []
 
-p0 = peaks.pop(0)
-while len(peaks) > 0:
-	p1 = peaks.pop(0)
+i = 1
+p0 = peaks[0]
+while i < len(peaks):
+	p1 = peaks[i]
+	i += 1
 
 	if np.abs(p1 - p0) <= dist:
 		new = round((p1 + p0) / 2)
 		peaks_merged.append(new)
-		p0 = peaks.pop(0)
+		p0 = peaks[0]
+		i += 1
 	else:
 		peaks_merged.append(p0)
 		p0 = p1
@@ -93,16 +91,52 @@ while len(peaks) > 0:
 # Append the final point
 peaks_merged.append(p0)
 
+# Take midpoints between peaks
+midpoints = []
+for p0, p1 in zip(peaks_merged[:-1], peaks_merged[1:]):
+	midpoints.append(int((p0 + p1) / 2))
+
+print(f"[*] Peak detection done")
+print(f"[*] Saving frames ...")
+
+# Save frames at the midpoints
+for idx, p in zip(range(len(midpoints)), midpoints):
+	cap.set(cv2.CAP_PROP_POS_FRAMES, p)
+
+	if idx % 100 == 0:
+		print(f"[*] Saving frame {idx:05d}/{len(midpoints):05d}")
+
+	# Get the frame
+	ret, frame = cap.read()
+
+	if ret == False:
+		print(f"[-] Error reading frame!")
+		quit()
+	
+	cv2.imwrite(f"frames/{idx}.png", frame)
+
 # Generate signal for graphing
+peaks_signal = np.zeros(len(filtered))
+for p in peaks:
+	peaks_signal[p] = 5
+
 peaks_merged_signal = np.zeros(len(filtered))
 for p in peaks_merged:
 	peaks_merged_signal[p] = 5
+
+midpoints_signal = np.zeros(len(filtered))
+for p in midpoints:
+	midpoints_signal[p] = 5
 
 #plt.plot(deltas)
 plt.plot(filtered)
 plt.plot(mvals)
 plt.plot(peaks_signal)
 plt.plot(peaks_merged_signal)
+plt.plot(midpoints_signal)
+
+plt.savefig('timing_recovery.png')
+
 plt.show()
 
 cap.release()
